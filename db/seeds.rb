037f286d33430cbd860c2c70a77600d5337c71de
yuya yamada <yuya19920721@gmail.com>
2021-05-26 03:45:45 +0900

    seedデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+properties = [
+  { name: 'ハイツA', rent: '10000', address: '兵庫県姫路市', age: '1', remarks: '立地が良い'},
+  { name: 'ハイツB', rent: '20000', address: '兵庫県明石市', age: '2', remarks: '中が綺麗'},
+  { name: 'ハイツC', rent: '30000', address: '兵庫県西宮市', age: '3', remarks: '庭がある'},
+  { name: 'ハイツD', rent: '40000', address: '兵庫県富岡市', age: '4', remarks: 'クーラー付き'},
+  { name: 'ハイツE', rent: '50000', address: '兵庫県西脇市', age: '5', remarks: '床暖房付いてます'},
+  { name: 'ハイツF', rent: '60000', address: '兵庫県三木市', age: '6', remarks: '駅から１分'},
+  { name: 'ハイツG', rent: '70000', address: '兵庫県宝塚市', age: '7', remarks: 'トイレと風呂が一緒'},
+  { name: 'ハイツH', rent: '80000', address: '兵庫県川西市', age: '8', remarks: '角部屋'},
+  { name: 'ハイツI', rent: '90000', address: '兵庫県高砂市', age: '9', remarks: '屋上自由'},
+  { name: 'ハイツJ', rent: '10000', address: '兵庫県尼崎市', age: '10', remarks: '無線LAN無料'}
+]
+Property.create! properties
